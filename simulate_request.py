91ad70b46a08bc47f21c1a29bb5b1a29af2428ff
blinda <linda.baro91@gmail.com>
2020-06-03 16:07:40 +0200

    Convert base64 to json and save in csv

--- simulate_request.py
+++ simulate_request.py
@@ -2,17 +2,28 @@
 Simulates an API request. The user needs to run simulate_API.py before
 to simulate the webpage.
 """
+import base64
+import zlib
 import pandas as pd
 import requests
 from flask import jsonify
 import json
 
-res = requests.get('http://127.0.0.1:5000/api/data')
-r = json.loads(res.text)
-print(r)
+# Read byte content
+coded = requests.get('http://127.0.0.1:5000/api/data')
 
-df = pd.read_json(r)
+# Decode from base64
+res = base64.b64decode(coded.text)
+# Decompress bytes
+res_exp = zlib.decompress(res)
+# Decode to utf-8
+res_str = res_exp.decode()
+# Convert to json
+r = json.loads(res_str)
 
+df = pd.DataFrame(r)
+
+# Save as csv
 df = df.set_index('ID')
 df = df.sort_index()
 df.to_csv("Patient.csv")
